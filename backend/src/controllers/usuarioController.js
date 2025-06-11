961f83830bd481078404ec4fec2d2ea37ab704ea
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-06-11 17:41:04 -0300

    adicionado: criação e remoção de interesses por usuário

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -3,7 +3,14 @@ import bcrypt from "bcrypt";
 
 export async function criarUsuario(req, res) {
   const usuario = req.body;
-  if (!usuario) return res.status(401).send("informe todos os  campos");
+  if (
+    !usuario ||
+    !usuario.nome ||
+    !usuario.email ||
+    !usuario.senha ||
+    !usuario.data_nascimento
+  )
+    return res.status(401).send("informe todos os  campos");
   const criarSQL =
     "INSERT INTO usuario(nome, email, senha, data_nascimento) values(?, ?, ?, ?)";
 
@@ -17,24 +24,27 @@ export async function criarUsuario(req, res) {
       usuario.data_nascimento,
     ]);
 
-    console.log(result);
     return res.status(201).send(result);
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Um erro inesperado ocorreu");
+    return res.status(500).send("Erro interno do servidor");
   }
 }
 
 export async function acharUsuario(req, res) {
   const email = req.params.email;
-  if (!email) return res.status(400).send("O email e a senha são obrigatórios");
+  const senha = req.params.senha;
   const acharSQL = "SELECT * from usuario where email = ?";
 
   try {
     const [result] = await pool.query(acharSQL, [email]);
-    console.log(result);
-    return res.status(200).send(result);
+    if (!result || !result.length)
+      return res.status(404).send("Usuário não encontrado");
+    if (await bcrypt.compare(senha, result[0].senha))
+      return res.status(200).send(result[0]);
+    return res.status(401).send("Senha incorreta");
   } catch (error) {
     console.error(error);
+    return res.status(500).send("Erro interno do servidor");
   }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import usuariosRoutes from "./routes/usuariosRoutes.js";
+import usuariosInteressesRoutes from "./routes/usuariosInteressesRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/usuario", usuariosRoutes);
+
+app.use("/usuarioInteresse", usuariosInteressesRoutes);
 
 app.get("/", (req, res) => res.send("Servidor rodando"));
 
