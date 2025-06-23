eb4f6626896ed8f9e2b443d9f98749f5630d3f4a
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-06-23 16:01:04 -0300

    fix: limites de conexões

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_ADDON_DB,
   port: process.env.MYSQL_ADDON_PORT,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 5,
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
