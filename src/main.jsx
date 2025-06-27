e6edebfbbaa5933ffc2b3a3ed7cd66bfa7a59021
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-06-27 16:32:24 -0300

    fix: basename error

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ createRoot(document.getElementById("root")).render(
         <PostagensProvider>
           <MensagensProvider>
             <ConexoesProvider>
-              <Router>
+              <Router basename="/LinkUS">
                 <Routes>
                   <Route path="/" element={<App />} />
                   <Route path="/entrar" element={<Entrar />} />
